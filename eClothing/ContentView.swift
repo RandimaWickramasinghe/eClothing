0880d2b7e09e559ea736f0fd39a346827f61e37a
RandimaWickramasinghe <cobsccomp222p-018@student.nibm.lk>
2024-03-28 23:58:07 +0530

    login page

--- eClothing/ContentView.swift
+++ eClothing/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userName = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.mint,.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Login")
+                    .font(.largeTitle)
+                    .bold()
+                    //.padding()
+                Spacer()
+                
+                TextField("Username", text: $userName)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(10)
+                
+            }
+            //Color(.mint)
+            
         }
-        .padding()
+        //.padding()
     }
 }
 
