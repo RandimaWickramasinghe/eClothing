ec5187ba0a7728b933bd91f5cc8c7c67cc3aaf5d
RandimaWickramasinghe <cobsccomp222p-018@student.nibm.lk>
2024-03-28 23:36:35 +0530

    Initial Commit

---
+++ eClothing/eClothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  eClothingApp.swift
+//  eClothing
+//
+//  Created by Semini Wickramasinghe on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct eClothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
